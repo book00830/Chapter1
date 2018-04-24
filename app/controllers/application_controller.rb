4f48a192190b4c38a4ecb61f60761fe7f5f74ad7
choe <chaegeun>
2018-04-24 17:59:09 +0900

    branch test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
     def hello
-      render html: "hello, world! ¡ Hola, mundo! bitbucketh"
+      render html: "hello, world! ¡ Hola, mundo! bitbucketh エラ：fatal: remote origin already exists."
     end
 end
