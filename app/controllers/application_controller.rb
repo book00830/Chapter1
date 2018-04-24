d6c433f2acddef199326e79ba28da99cfd5aaa6a
choe <chaegeun>
2018-04-24 17:38:16 +0900

    bitbuckettest3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
     def hello
-      render html: "hello, world! ¡ Hola, mundo! bitbucketh  hi"
+      render html: "hello, world! ¡ Hola, mundo! bitbucketh"
     end
 end
